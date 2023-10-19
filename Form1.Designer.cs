ba9cc6fb828450a1c677ae9fce2b5e232208ca80
lucas fonseca <luzkfonseca@gmail.com>
2023-10-18 22:34:28 -0300

    primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace CalculadoraJuros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.txtTaxaJuros = new System.Windows.Forms.TextBox();
+            this.txtNumeroParcelas = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblValorParcela = new System.Windows.Forms.Label();
+            this.lblTotalJuros = new System.Windows.Forms.Label();
+            this.lblTotalPago = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Location = new System.Drawing.Point(53, 84);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(158, 20);
+            this.txtPrincipal.TabIndex = 0;
+            // 
+            // txtTaxaJuros
+            // 
+            this.txtTaxaJuros.Location = new System.Drawing.Point(53, 131);
+            this.txtTaxaJuros.Name = "txtTaxaJuros";
+            this.txtTaxaJuros.Size = new System.Drawing.Size(158, 20);
+            this.txtTaxaJuros.TabIndex = 1;
+            // 
+            // txtNumeroParcelas
+            // 
+            this.txtNumeroParcelas.Location = new System.Drawing.Point(53, 181);
+            this.txtNumeroParcelas.Name = "txtNumeroParcelas";
+            this.txtNumeroParcelas.Size = new System.Drawing.Size(158, 20);
+            this.txtNumeroParcelas.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(246, 119);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(62, 33);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblValorParcela
+            // 
+            this.lblValorParcela.AutoSize = true;
+            this.lblValorParcela.Location = new System.Drawing.Point(350, 86);
+            this.lblValorParcela.Name = "lblValorParcela";
+            this.lblValorParcela.Size = new System.Drawing.Size(76, 13);
+            this.lblValorParcela.TabIndex = 4;
+            this.lblValorParcela.Text = "Valor Parcela: ";
+            // 
+            // lblTotalJuros
+            // 
+            this.lblTotalJuros.AutoSize = true;
+            this.lblTotalJuros.Location = new System.Drawing.Point(350, 135);
+            this.lblTotalJuros.Name = "lblTotalJuros";
+            this.lblTotalJuros.Size = new System.Drawing.Size(59, 13);
+            this.lblTotalJuros.TabIndex = 5;
+            this.lblTotalJuros.Text = "Total Juros";
+            // 
+            // lblTotalPago
+            // 
+            this.lblTotalPago.AutoSize = true;
+            this.lblTotalPago.Location = new System.Drawing.Point(350, 184);
+            this.lblTotalPago.Name = "lblTotalPago";
+            this.lblTotalPago.Size = new System.Drawing.Size(59, 13);
+            this.lblTotalPago.TabIndex = 6;
+            this.lblTotalPago.Text = "Total Pago";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Calculadora de Juros/Financiamento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Valor Principal do Financiamento";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Taxa de Juros Anual";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Número de Parcelas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(580, 256);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotalPago);
+            this.Controls.Add(this.lblTotalJuros);
+            this.Controls.Add(this.lblValorParcela);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumeroParcelas);
+            this.Controls.Add(this.txtTaxaJuros);
+            this.Controls.Add(this.txtPrincipal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.TextBox txtTaxaJuros;
+        private System.Windows.Forms.TextBox txtNumeroParcelas;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblValorParcela;
+        private System.Windows.Forms.Label lblTotalJuros;
+        private System.Windows.Forms.Label lblTotalPago;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraJuros
+{
+    public partial class Form1 : Form
+    {   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtPrincipal.Text, out double principal) &&
+        double.TryParse(txtTaxaJuros.Text, out double taxaJuros) &&
+        int.TryParse(txtNumeroParcelas.Text, out int numeroParcelas))
+            {
+                double taxaJurosMensal = (taxaJuros / 100) / 12;
+                double valorParcela = principal * (taxaJurosMensal / (1 - Math.Pow(1 + taxaJurosMensal, -numeroParcelas)));
+                double totalJuros = (valorParcela * numeroParcelas) - principal;
+                double totalPago = principal + totalJuros;
+
+                lblValorParcela.Text = $"Valor da parcela: {valorParcela:C}";
+                lblTotalJuros.Text = $"Total em juros: {totalJuros:C}";
+                lblTotalPago.Text = $"Total Pago: {totalPago:C}";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira valores válidos.");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
